2da932de3ad68db27fe4927fc59aebd7a5001f03
osamaayyad99 <osama.ayyad.99@gmail.com>
2020-11-07 13:32:24 +0200

    make a java script library

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 let allBooks = localStorage.getItem("books");
-let books = allBooks ? allBooks : [
+let books =  [
             {title: 'A Smarter Way to Learn JavaScript', author: 'Mark Myers', image: './assets/images/1.jpg'},
             {title: 'Eloquent JavaScript', author: 'Marjin Haverbeke', image: './assets/images/2.jpg'},
             {title: 'JavaScript & JQuery: Interactive Front-End Web Development', author: 'Jon Duckett', image: './assets/images/3.png'},
-            {title: 'JavaScript: The Good Parts', author: 'Douglas Crockford', image: '4.jpj'},
-            {title: 'Learn JavaScript VISUALLY', author: 'Ivelin Demirov', image: '5.png'},
+            {title: 'JavaScript: The Good Parts', author: 'Douglas Crockford', image: './assets/images/4.jpg'},
+            {title: 'Learn JavaScript VISUALLY', author: 'Ivelin Demirov', image: './assets/images/5.png'},
             {title: 'JavaScript: The Definitive Guide', author: 'David Flanagan', image: './assets/images/6.jpg'},
             {title: 'Effective JavaScript', author: 'David Herman, Foreword by Brendan Eich', image: './assets/images/7.png'},
             {title: 'JavaScript for Kids', author: 'Nick Morgan', image: './assets/images/8.png'},
@@ -12,10 +12,7 @@ let books = allBooks ? allBooks : [
             {title: 'High-Performance Browser Networking', author: 'Ilya Grigorik', image: './assets/images/10.jpg'}
 ];
 
-
-
-let card = document.getElementsByClassName("cards");
-
+let card = document.getElementById("cards");
 
 /*let someBooks = [{title: 'A Smarter Way to Learn JavaScript', author: 'Mark Myers', image: './assets/images/1.jpg'},
                  {title: 'Eloquent JavaScript', author: 'Marjin Haverbeke', image: './assets/images/2.jpg'},
@@ -36,13 +33,13 @@ for(var key in someBooks) {
 showBooks();
 function showBooks(){
     card.innerHTML = "";
-  
-    books.map(function (book,i){
-        let cardItem = document.createElement("div").className = "cardItem";
-        let cardContent = document.createElement("div").className = "cardContent";
-        let bookTitle = document.createElement("div").className = "cardTitle";
-        let bookAuthor = document.createElement("div").className = "cardAuthor";
-        let bookImage = document.createElement("img").className = "cardImage";
+    //let myBooks= JSON.parse(books);
+        books.map(function (book,i){
+        let cardItem = document.createElement("div");
+        let cardContent = document.createElement("div");
+        let bookTitle = document.createElement("div");
+        let bookAuthor = document.createElement("div");
+        let bookImage = document.createElement("img");
         bookTitle.textContent = book.title;
         bookAuthor.textContent = book.author;
         bookImage.src= book.image;
@@ -51,12 +48,15 @@ function showBooks(){
         cardContent.appendChild(bookAuthor);
         cardItem.appendChild(cardContent);
         card.appendChild(cardItem);
-
-
+        cardItem.className = 'cardItem';
+        cardContent.className = 'cardContent';
+        bookTitle.className = 'cardTitle';
+        bookAuthor.className = 'cardAuthor';
+        bookImage.className = 'cardImage';
     }
     );
 
-    localStorage.setItem("books", JSON.stringify(books));
+   // localStorage.setItem("books", JSON.stringify(books));
 }
 
 document.getElementById("add-btn").addEventListener("click", function(){
@@ -79,5 +79,7 @@ document.getElementById("add-btn").addEventListener("click", function(){
         document.getElementById("Bauthor").value="";
         document.getElementById("Bimg").value="";
         showBooks();
+        
     }
+    
 });
